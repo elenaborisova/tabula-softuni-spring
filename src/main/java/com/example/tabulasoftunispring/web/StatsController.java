53d6db2ff9754194188bbccc472f0c731daef4bb
Elena Borisova <elenaborrisova@gmail.com>
2022-09-25 22:59:39 +0200

    forbid access to stats page

--- src/main/java/com/example/tabulasoftunispring/web/StatsController.java
+++ src/main/java/com/example/tabulasoftunispring/web/StatsController.java
@@ -2,12 +2,14 @@ package com.example.tabulasoftunispring.web;
 
 import com.example.tabulasoftunispring.services.StatsService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @AllArgsConstructor
 @Controller
+@PreAuthorize("hasRole('ADMIN')")
 public class StatsController {
 
     private final StatsService statsService;
